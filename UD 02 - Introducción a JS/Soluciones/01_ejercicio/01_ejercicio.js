1b94c4935dbafe965ca6dfc1f984482760ce59d1
borjasaizbustamante <56587541+borjasaizbustamante@users.noreply.github.com>
2026-08-20 18:36:25 +0200

    Upload

--- UD 02 - Introducción a JS/Soluciones/01_ejercicio/01_ejercicio.js
+++
@@ -1,9 +0,0 @@
-var nombre = window.prompt("Nombre: ");
-var ape1 = window.prompt("Primer Apellido: ");
-var ape2 = window.prompt("Segundo Apellido: ");
-
-document.write("<p>Nombre: " + nombre + "</p>");
-document.write("<p>Primer Apellido: " + ape1 + "</p>");
-document.write("<p>Segundo Apellido: " + ape2 + "</p>");
-
-document.write("<p><b>" + nombre + ape1 + ape2 + "</b></p>");
